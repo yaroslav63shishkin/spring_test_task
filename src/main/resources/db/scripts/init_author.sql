f75820c36d943a4ef6623dd02c4645c8fb3372d7
YaroslavShishkin <103732435+YaroslavShishkin@users.noreply.github.com>
2022-11-23 13:07:44 +0400

    database created and loaded initial data

---
+++ src/main/resources/db/scripts/init_author.sql
@@ -0,0 +1,4 @@
+insert into author (name, surname, middle_name, date_of_birth, creation_date, update_date)
+values ('Василий', 'Васильев', 'Васильевич', '1999-01-08', current_timestamp, current_timestamp),
+       ('Василий', 'Петров', 'Васильевич', '1999-01-08', current_timestamp, current_timestamp),
+       ('Василий', 'Иванов', 'Васильевич', '1999-01-08', current_timestamp, current_timestamp);
---
+++ src/main/resources/db/scripts/init_genre.sql
@@ -0,0 +1,5 @@
+insert into genre (description, creation_date, update_date)
+values ('Фантастика', current_timestamp, current_timestamp),
+       ('Детектив', current_timestamp, current_timestamp),
+       ('Роман', current_timestamp, current_timestamp),
+       ('Драма', current_timestamp, current_timestamp);
