8281df04cddf1da1bcdcd4c265bf1b4b9965b981
YaroslavShishkin <103732435+YaroslavShishkin@users.noreply.github.com>
2022-11-24 16:32:41 +0400

    created custom controller and ResponseControllerAdvice

---
+++ src/main/java/com/example/spring_test_task/controller/ResponseControllerAdvice.java
@@ -0,0 +1,10 @@
+package com.example.spring_test_task.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ResponseControllerAdvice {
+
+}
